7917e7d49cf8594e9ed779cc68dc213e9c2bb84e
Mateus8637228 <matrac2k20@gmail.com>
2025-10-06 02:02:23 +0700

    labubushka.cpp

--- labubushka.cpp
+++ labubushka.cpp
@@ -10,18 +10,18 @@
 using namespace std;
 
 int main() {
-
-  double tR; tB; nuCm2S; nu; g; d; deltaT; Gr; Nu;
-  tR = 500.0
+  double tR, tB, nuCm2S, nu, g, d, deltaT, Gr, Nu;
+  
+  tR = 500.0;
   tB = 15.0;
-  nu_cm2_s = 0.15;
-  nu = nu_cm2_s * 0.0001;
+  nuCm2S = 0.15;
+  nu = nuCm2S * 0.0001;
   g = 9.81;
-  d = 0.1
 
   cout << fixed << setprecision(1);
   cout << "m    Gr          Nu" << endl;
 
+  d = 0.1;
   while (d <= 0.3) {
     deltaT = tR - tB;
     Gr = g * pow(d, 3.0) * deltaT / (nu * nu * (tB + 273.0));
@@ -34,11 +34,18 @@ int main() {
 
     cout << d << "  " << Gr << "  " << Nu << endl;
 
-    if (d == 0.1) d = 0.2;
-    else if (d == 0.2) d = 0.25;
-    else if (d == 0.25) d = 0.275;
-    else if (d == 0.275) d = 0.3;
-    else d += 1;
+    if (d == 0.1) {
+      d = 0.2;
+    } else if (d == 0.2) {
+      d = 0.25;
+    } else if (d == 0.25) {
+      d = 0.275;
+    } else if (d == 0.275) {
+      d = 0.3;
+    } else {
+      d = 0.4;
+    }
   }
+  
   return 0;
 }
